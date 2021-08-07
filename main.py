ba1e9b23ccef873b21d24ebfcc184ee2cc541a19
Anthony Teixeira <35811311+Atrone@users.noreply.github.com>
2021-08-07 17:05:14 -0400

    major refactor

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import json
+import os
+
 import historicalCryptoTrendsGenerator
 import cryptoInvestmentAdvisor
 import tools
+from datetime import datetime
+import pymongo
+
 
 def createResponseCSVs(generate,startDate,hours,cryptoLimit,dollars):
     if(generate):
-        cryptoInvestmentAdvisor.getResults(historicalCryptoTrendsGenerator.getResults(
-            startDate,hours,cryptoLimit)
-                                           ,dollars)
-    else:
-        cryptoInvestmentAdvisor.getResults(tools.generateHistoricalCryptoTrendsGeneratorResultsFromCSV(startDate)
-                                           ,dollars)
+        cryptoInvestmentAdvisor.Executor(historicalCryptoTrendsGenerator.Executor(
+            startDate,hours,cryptoLimit,"desc").getResults(),0,dollars).getResults()
+
+if __name__ == "__main__":
+    print('creating CSVs')
+    #createResponseCSVs()
